7e58ecf138aaa350c451189b83de32b9b922e330
tomoki-kimura <moxon.zeus@gmail.com>
2018-12-26 14:36:36 +0900

    2018年 12月26日 水曜日 14時36分36秒 JST

--- git_merge_all.sh
+++ git_merge_all.sh
@@ -3,8 +3,8 @@
 git add *
 git commit -m "`date`"
 git push
-git checkout master
-git merge kimura -m "`date`"
-git commit -m "`date`"
-git push
-git checkout kimura
+#git checkout master
+#git merge kimura -m "`date`"
+#git commit -m "`date`"
+#git push
+#git checkout kimura
